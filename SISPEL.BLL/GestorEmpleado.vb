badf5ba59b0b8ac28ac8c098ed98cd891acc5358
julianmartinre <julian.martin.93@gmail.com>
2017-09-18 08:02:26 -0300

    Se agregó GestorEmpleado

---
+++ SISPEL.BLL/GestorEmpleado.vb
@@ -0,0 +1,21 @@
+﻿Imports SISPEL.Biz.Entities
+Imports SISPEL.DAL.DAL
+
+Namespace BLL
+
+    Public Class GestorEmpleado
+        Public Sub insertarEmpleado(Empleado As Empleado)
+
+            If Empleado.nombre = "" Or Empleado.apellido = "" Or Empleado.dni = Nothing Then
+
+                Throw New Exception
+
+            End If
+
+            Dim empleadoDAL As New EmpleadoDAL
+            empleadoDAL.insertarEmpleado(Empleado)
+
+        End Sub
+    End Class
+
+End Namespace
