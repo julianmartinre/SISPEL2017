c277445eafc4231dfa77c3ed266c8ecd5715f9bf
julianmartinre <julian.martin.93@gmail.com>
2017-09-19 12:10:16 -0300

    Se agregó gestión de backup.

--- SISPEL/My Project/Application.Designer.vb
+++ SISPEL/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SISPEL.frmLogin
+            Me.MainForm = Global.SISPEL.frmBackup
         End Sub
     End Class
 End Namespace
