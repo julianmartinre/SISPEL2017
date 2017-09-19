bdaffe7147e949cbcc034a9dda235f7bfaee3238
julianmartinre <julian.martin.93@gmail.com>
2017-09-19 13:59:35 -0300

    Implementación de multi-idioma en LogIn.

--- SISPEL/My Project/Application.Designer.vb
+++ SISPEL/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SISPEL.frmBackup
+            Me.MainForm = Global.SISPEL.frmLogin
         End Sub
     End Class
 End Namespace
--- SISPEL/frmLogIn/frmLogin.vb
+++ SISPEL/frmLogIn/frmLogin.vb
@@ -1,11 +1,53 @@
 ﻿Imports RodriguezEscobedo.Framework
 Imports RodriguezEscobedo.Framework.Seguridad
 Imports SISPEL.Biz.Entities
+Imports SISPEL.BLL
 Imports SISPEL.BLL.BLL
 
 Public Class frmLogin
+    Implements IObservador
 
+    Dim mTraductor As Traductor = Traductor.GetInstance
     Dim mBitacora As Bitacora = Bitacora.GetInstance
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        For Each mControl As Control In Me.Controls
+            Try
+                CargarTags(mControl)
+            Catch ex As Exception
+
+            End Try
+        Next
+    End Sub
+
+    Public Sub Actualizar(pObservador As Control) Implements IObservador.Actualizar
+        For Each mControl As Control In pObservador.Controls
+            Try
+                mControl.Text = mTraductor.IdiomaSeleccionado.Diccionario.Item(mControl.Tag)
+            Catch ex As Exception
+
+            Finally
+                If mControl.Controls.Count > 0 Then
+                    Actualizar(mControl)
+                End If
+            End Try
+        Next
+    End Sub
+
+    Public Sub CargarTags(pControl As Control)
+        pControl.Tag = pControl.Text
+
+        If pControl.Controls.Count > 0 Then
+            For Each mControl As Control In pControl.Controls
+                CargarTags(mControl)
+            Next
+        End If
+    End Sub
 
     Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
 
@@ -25,6 +67,14 @@ Public Class frmLogin
             End If
 
         End If
+
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        mTraductor.RegistrarObservador(Me)
+
+        Actualizar(Me)
 
     End Sub
 
